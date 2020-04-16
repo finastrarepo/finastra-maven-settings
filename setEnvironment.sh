b2fdb0eb63e507e96e9b802a156cc5dc48985bcd
Kalidass <kalidass+gitkraken@gmail.com>
2020-04-16 18:32:08 +0100

    added application environment and application name appender

--- setEnvironment.sh
+++ setEnvironment.sh
@@ -6,14 +6,22 @@ set -e
 case "$TRAVIS_BRANCH" in
     "develop")
         export anypoint_environment="Development"
+        export anypoint_application_environment="dev"
+        export anypoint_application_name_appender="-dev"
     ;;
     "integrate")
         export anypoint_environment="SIT"
+        export anypoint_application_environment="sit"
+        export anypoint_application_name_appender="-sit"
     ;;
     "release")
         export anypoint_environment="UAT"
+        export anypoint_application_environment="uat"
+        export anypoint_application_name_appender="-uat"
     ;;
     "master")
         export anypoint_environment="Production"
+        export anypoint_application_environment="prod"
+        export anypoint_application_name_appender=""
     ;;
 esac
