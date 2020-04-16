aaf4e2a78ee336151ac6da268bfb4e6a560f9cab
Kalidass <kalidass+gitkraken@gmail.com>
2020-04-16 17:15:49 +0100

    changed to export statement

--- setEnvironment.sh
+++ setEnvironment.sh
@@ -3,18 +3,17 @@
 #To abort if any command in script exit with error status
 set -e
 
-anypoint_environment=""
 case "$TRAVIS_BRANCH" in
     "develop")
-        anypoint_environment="Development"
+        export anypoint_environment="Development"
     ;;
     "integrate")
-        anypoint_environment="SIT"
+        export anypoint_environment="SIT"
     ;;
     "release")
-        anypoint_environment="UAT"
+        export anypoint_environment="UAT"
     ;;
     "master")
-        anypoint_environment="Production"
+        export anypoint_environment="Production"
     ;;
 esac
