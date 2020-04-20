a4a74eb1cc864bedab49e6ff84102c89608358d6
Kalidass <kalidass+gitkraken@gmail.com>
2020-04-20 12:41:04 +0100

    included anypoint maven additional parameters

--- setEnvironment.sh
+++ setEnvironment.sh
@@ -8,20 +8,24 @@ case "$TRAVIS_BRANCH" in
         export anypoint_environment="Development"
         export anypoint_application_environment="dev"
         export anypoint_application_name_appender="-dev"
+        export anypoint_maven_additional_parameters="-DskipMunitTests"
     ;;
     "integrate")
         export anypoint_environment="SIT"
         export anypoint_application_environment="sit"
         export anypoint_application_name_appender="-sit"
+        export anypoint_maven_additional_parameters="-DskipMunitTests"
     ;;
     "release")
         export anypoint_environment="UAT"
         export anypoint_application_environment="uat"
         export anypoint_application_name_appender="-uat"
+        export anypoint_maven_additional_parameters="-DskipMunitTests"
     ;;
     "master")
         export anypoint_environment="Production"
         export anypoint_application_environment="prod"
         export anypoint_application_name_appender=""
+        export anypoint_maven_additional_parameters="-DskipMunitTests"
     ;;
 esac
